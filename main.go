30aa7ba134dae3a268b9daf1a7a361de628a12e9
Rafael <rafaelcnunes@icloud.com>
2024-06-09 23:42:44 -0300

    implements backend for products

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+type Product struct {
+	Name, Description string
+	Price             float64
+	Quantity          int
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	products := []Product{
+		{"Product 1", "Description 1", 1.99, 10},
+		{"Product 2", "Description 2", 2.99, 20},
+		{"Product 3", "Description 3", 3.99, 30},
+		{"Product 4", "Description 4", 4.99, 40},
+	}
+
+	err := templates.ExecuteTemplate(w, "Index", products)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil).Error()
+}
